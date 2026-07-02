c05bf07a8d0b8a87502f275eef44534fe808e675
divyanhiupreti11 <upretidivyanshi@gmail.com>
2026-07-02 12:48:20 +0530

    Update CORS configuration for deployment

--- HireSense/Backend/src/app.js
+++ HireSense/Backend/src/app.js
@@ -7,12 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-    ],
-    credentials: true
-}))
+  origin:true,
+  credentials: true
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
